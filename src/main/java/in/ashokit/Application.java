35d30618754fd76956166c8c1d713f7f70b4abd0
Niketan <nickthombare@gmail.com>
2022-08-12 15:07:14 +0530

    SLA-200:variable is added in start class

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
             private Static final String url="";
+            private int value1;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
